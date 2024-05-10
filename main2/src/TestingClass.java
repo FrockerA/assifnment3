e306e4226ca0dcd514db87015d098cb9cb4f0575
FrockerA <161676679+FrockerA@users.noreply.github.com>
2024-05-11 04:52:52 +0600

    Update TestingClass.java

--- main2/src/TestingClass.java
+++ main2/src/TestingClass.java
@@ -7,14 +7,12 @@ public class TestingClass {
         MyHashTable<MyTestingClass, Student> table = new MyHashTable<>();
         Random random = new Random();
 
-        // Adding random elements to MyHashTable
         for (int i = 0; i < 10000; i++) {
             MyTestingClass key = new MyTestingClass(random.nextInt(1000));
             Student value = new Student("Student " + i);
             table.put(key, value);
         }
 
-        // Printing number of elements in each bucket
         MyHashTable.HashNode<MyTestingClass, Student>[] chainArray = table.getChainArray();
         for (int i = 0; i < chainArray.length; i++) {
             int count = 0;
@@ -29,38 +27,31 @@ public class TestingClass {
         // Testing BST
         BST<Integer, String> bst = new BST<>();
 
-        // Adding random elements to BST
         for (int i = 0; i < 10000; i++) {
             int key = random.nextInt(1000);
             String value = "Value " + i;
             bst.put(key, value);
         }
 
-        // Printing the size of BST
         System.out.println("Size of BST: " + bst.size());
 
-        // Iterating through BST and printing key-value pairs
         for (Integer key : bst.iterator()) {
             System.out.println("Key is " + key + " and value is " + bst.get(key));
         }
     }
 
-    // Define MyTestingClass here with its own hashCode() method
     static class MyTestingClass {
         int value;
-
         public MyTestingClass(int value) {
             this.value = value;
         }
 
         @Override
         public int hashCode() {
-            // Your custom hash code implementation
             return value;
         }
     }
 
-    // Define Student class here (assuming it's a simple class with a name field)
     static class Student {
         String name;
 
